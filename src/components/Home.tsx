f8bb5d6e486424de9496a53c4bb62ad521718d69
DEVLevid <levidlima80@gmail.com>
2024-06-02 20:46:38 -0300

    refactor: refactoring Home component.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,29 @@
 import { PaperPlaneTilt, ReadCvLogo } from "@phosphor-icons/react";
 import Marquee from "react-fast-marquee";
 import cv from '../assets/cv.pdf'
+import icon from '../assets/Code typing-bro.png'
 
 export default function Home() {
   return (
     <>
-      <div className="flex items-center mt-40">
-        <div className="flex flex-col ml-60">
+      <div id="#home" className="flex items-center mt-60">
+        <div className="flex flex-col ml-80">
           <h1 className=" text-6xl font-bold">
-            👾 <span className=" text-purple-800">Olá!!</span> Sou Levid
+            👾 Olá!! Sou <span className=" text-purple-800"> Levid </span>
           </h1>
           <div className=" flex flex-row items-center gap-4 mt-6">
             <p className=" border-b-2 w-64"></p>
             <p className=" flex text-center text-4xl gap-2 font-bold">
-              Desenvolvedor<span className=" text-purple-800">Frontend</span>
+              Desenvolvedor<span className=" text-purple-800">Frontend </span>
             </p>
           </div>
-          <div className=" w-3/4 mt-4 font-medium text-wrap text-xl leading-8">
+          <div className=" w-2/4 mt-4 font-medium text-wrap text-xl leading-8">
             <p>
-              Desenvolvedor Frontend apaixonado por tecnologia e por transformar
+              <span className="text-purple-300">Desenvolvedor Frontend</span> apaixonado por tecnologia e por transformar
               ideias em realidade através dela! Aqui você encontra alguns dos
               meus trabalhos. Se você deseja transformar sua ideia em um produto
               original e de qualidade,
-              <span className="text-purple-300"> entre em contato</span> comigo!
+              <span className="text-purple-300"> entre em contato comigo! </span>
             </p>
           </div>
           <div className="flex flex-row gap-6 mt-6 items-center">
@@ -34,53 +35,35 @@ export default function Home() {
             </button></a>
           </div>
         </div>
-        <div className="flex items-center justify-center mr-60 rounded-lg object-cover overflow-hidden">
+        <div className="flex items-center justify-center rounded-lg object-cover overflow-hidden absolute right-80 w-80">
           <img
-            src="https://github.com/DEVLevid.png"
+            src={icon}
             alt=""
-            className="object-cover grayscale"
+            className="object-cover"
           />
         </div>
       </div>
 
       <Marquee
         direction="left"
-        speed={100}
+        speed={80}
         autoFill
-        className="flex bg-gray-900 h-16 mt-40 w-10/12 rounded-md m-auto"
+        className="flex bg-black h-20 mt-12 w-11/12 rounded-md m-auto items-center flex-row gap-6 transform -rotate-[2deg]"
+        style={{ transformOrigin: 'center' }}
       >
-        <div className="flex flex-row gap-4 ">
-          <span>Reactjs</span>
-          <span>/</span>
-          <span>NodeJs</span>
-          <span>✧</span>
-          <span>Typescript</span>
-          <span>✧</span>
-          <span>Tailwind Css</span>
-          <span>✧</span>
-          <span>NextJs</span>
-          <span>✧</span>
-          <span>HTML5</span>
-          <span>✧</span>
-          <span>SASS | SCSS</span>
-          <span>✧</span>
-          <span>Rest API</span>
-          <span>✧</span>
-          <span>CSS3</span>
-          <span>✧</span>
-          <span>Javasript</span>
-          <span>✧</span>
-          <span>Jest</span>
-          <span>✧</span>
-          <span>Jquery</span>
-          <span>✧</span>
-          <span>React Query</span>
-          <span>✧</span>
-          <span>Axios</span>
-          <span>✧</span>
-          <span>Git</span>
-          <span>✧</span>
-        </div>
+          <img src="https://img.icons8.com/?size=100&id=bzf0DqjXFHIW&format=png&color=000000" alt="ReactJs" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=54087&format=png&color=000000" alt="Node" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=uJM6fQYqDaZK&format=png&color=000000" alt="Typescript" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000" alt="Tailwind css" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=yUdJlcKanVbh&format=png&color=000000" alt="NextJs" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=20909&format=png&color=000000" alt="HTML5" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=21278&format=png&color=000000" alt="css3" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=QBqFNfPPB2Kx&format=png&color=000000" alt="sass | scss" className="size-8" />
+          <img src="https://img.icons8.com/?size=100&id=21894&format=png&color=000000" alt="api" className="size-8 mr-6" />
+          <img src="https://img.icons8.com/?size=100&id=PXTY4q2Sq2lG&format=png&color=000000" alt="Javascript" className="size-8 mr-8" />
+          <img src="https://img.icons8.com/?size=100&id=bp24DwGXJDyT&format=png&color=000000" alt="Jest" className="size-8" />
+          <img src="https://img.icons8.com/?size=100&id=40253&format=png&color=000000" alt="Jquery" className="size-8" />
+          <img src="https://img.icons8.com/?size=100&id=20906&format=png&color=000000" alt="Git" className="size-8 mr-6" />
       </Marquee>
     </>
   );
