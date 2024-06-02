db4b5c0d62164f4ffa782ee18a225540546cd674
DEVLevid <levidlima80@gmail.com>
2024-06-02 02:05:20 -0300

    refactor: Updating navBar styles.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
-import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
+import { Code, GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
 
 export default function Header() {
     return (
-        <header className="w-full h-16 bg-black flex items-center justify-between pl-12 pr-12">
-            <a href="/" className="text-2xl">Lima</a>
+        <header className="w-full h-14 bg-black flex items-center justify-between pl-72 pr-72">
+            <a href="/" className="text-2xl flex flex-row items-center gap-2"><Code size={28} />Lima</a>
             <ul className="text-center flex flex-row gap-20 text-xl">
-                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Home</a></li>
-                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">About</a></li>
-                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Projects</a></li>
-                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Contact</a></li>
+                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Início</a></li>
+                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Sobre mim</a></li>
+                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Projetos</a></li>
+                <li><a href="" className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Contato</a></li>
             </ul>
             <div className="flex flex-row gap-2">
                 <a href="https://github.com/DEVLevid"><GithubLogo size={32} className="hover:text-purple-600 cursor-pointer transition-colors delay" /></a>
