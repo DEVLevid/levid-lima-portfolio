7b9c8f6e25018a5bd4aa0c1e09db4a4a526034a1
DEVLevid <levidlima80@gmail.com>
2024-06-07 16:53:44 -0300

    feat: Creating portfolio section.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import About from "../components/About";
 import Header from "../components/Header";
 import Home from "../components/Home";
 import Services from "../components/Services";
+import Portfolio from "../components/portfolio";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Home />
       <About />
       <Services />
+      <Portfolio />
     </div>
   )
 }
---
+++ src/components/portfolio.tsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  FlowArrow,
+} from "@phosphor-icons/react";
+import { Projects } from "../data/projects";
+import "swiper/css/bundle";
+
+export default function Portfolio() {
+  return (
+    <div className="mb-40">
+      <h1 className="text-5xl flex mb-20 justify-center">Projetos</h1>
+      <Swiper
+        slidesPerView={3.2}
+        grabCursor
+        loop={true}
+        className="flex h-96 mb-12"
+      >
+        {Projects.map((data, index) => (
+          <div key={`${data.id} - ${index}`} className="flex flex-row">
+            <SwiperSlide className="flex">
+              <div
+                className="relative flex flex-col w-3/4 h-96 bg-cover bg-center p-8 rounded-lg"
+                style={{
+                  backgroundImage: data.pictureUrl
+                    ? `url(${data.pictureUrl})`
+                    : "none",
+                }}
+              >
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-90"></div>
+
+                <a href={data.link} className="absolute right-12 z-10">
+                  <FlowArrow
+                    size={32}
+                    className="text-gray-950 hover:text-purple-400 transition-all"
+                  />
+                </a>
+                <div className="z-10 absolute bottom-6">
+                  <h1 className="text-2xl font-mono mb-4 text-white">
+                    {data.name}
+                  </h1>
+                  <p className="font-light w-60 text-white">
+                    {data.description}
+                  </p>
+                </div>
+              </div>
+            </SwiperSlide>
+          </div>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
