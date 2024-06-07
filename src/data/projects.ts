01479b04dfc0d648a6ed2475c87c36c343490a7b
DEVLevid <levidlima80@gmail.com>
2024-06-07 16:53:28 -0300

    feat: Adding data projects for portfolio section.

---
+++ src/data/projects.ts
@@ -0,0 +1,35 @@
+import dolceImage from '../assets/dolce.png';
+import starImage from '../assets/star.png';
+import supiImage from '../assets/supi.jpeg';
+
+export const Projects = [
+  {
+    id: 1,
+    name: "Dolce Vita Bakery",
+    description: "Dolce Vita Bakery é um website desenvolvido para uma doceria italiana, criado como parte de um projeto freelancer.",
+    pictureUrl: dolceImage,
+    link: "https://dolce-vita-bakery.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Starbucks landing page",
+    description: "Uma landing page interativa para a cafeteria mais conhecida do mundo, nesse projeto demonstro minhas habilidades com DOM manipulation.",
+    pictureUrl: starImage,
+    link: "https://starbucks-landing-page-beta.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Supi chess",
+    description: "Uma plataforma de cursos online do grão-mestre de xadrez Luiz Paulo Supi e a Mestre-internacional Julia Alboredo.",
+    pictureUrl: supiImage,
+    link: "",
+  },
+  {
+    id: 4,
+    name: "Colabora - DIT",
+    description: "Contribuo ativamente no projeto COLABORA, uma aplicação Web para gestão interna.",
+    pictureUrl: "",
+    link: "",
+  },
+  
+];
