0ee2acafc09b9c2486a1e6af2226551fede052c4
DEVLevid <levidlima80@gmail.com>
2024-06-11 03:13:32 -0300

    fix: Fixing textarea input.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,7 @@
+import { useRef, FormEvent } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import emailjs from "@emailjs/browser";
+import "react-toastify/dist/ReactToastify.css";
 import {
   CaretCircleDoubleRight,
   EnvelopeOpen,
@@ -5,10 +9,6 @@ import {
   InstagramLogo,
   LinkedinLogo,
 } from "@phosphor-icons/react";
-import emailjs from "@emailjs/browser";
-import "react-toastify/dist/ReactToastify.css";
-import { useRef, FormEvent } from "react";
-import { toast, ToastContainer } from "react-toastify";
 
 export default function Contact() {
   const form = useRef<HTMLFormElement>(null);
@@ -19,17 +19,18 @@ export default function Contact() {
     });
   };
 
-  const sendEmail = (event: FormEvent) => {
+  const sendEmail = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (form.current) {
+      const formData = new FormData(form.current);
+      const emailData: Record<string, string> = {};
+      formData.forEach((value, key) => {
+        emailData[key] = value as string;
+      });
+
       emailjs
-        .sendForm(
-          "service_lne4vid",
-          "template_osu7n0c",
-          form.current,
-          "LGR-PTheaEtY-g2KA"
-        )
+        .send("service_j2am4vb", "template_osu7n0c", emailData, "LGR-PTheaEtY-g2KA")
         .then(
           (result) => {
             console.log(result.text);
@@ -45,7 +46,10 @@ export default function Contact() {
   };
 
   return (
-    <div id="contact" className="flex flex-col items-center justify-center mb-40">
+    <div
+      id="contact"
+      className="flex flex-col items-center justify-center mb-40"
+    >
       <h1 className="text-6xl mb-12">Entre em contato</h1>
       <div className="bg-gray-900 w-2/3 m-auto rounded-lg p-16 flex justify-between">
         <div className="flex flex-col w-2/4">
@@ -61,13 +65,22 @@ export default function Contact() {
 
           <h1 className="text-3xl font-mono mt-8">Social</h1>
           <div className="flex flex-row mt-4 gap-4">
-            <a href="https://www.instagram.com/levidlima/" className="hover:text-purple-900 transition-all">
+            <a
+              href="https://www.instagram.com/levidlima/"
+              className="hover:text-purple-900 transition-all"
+            >
               <InstagramLogo size={32} />
             </a>
-            <a href="https://github.com/DEVLevid" className="hover:text-purple-900 transition-all">
+            <a
+              href="https://github.com/DEVLevid"
+              className="hover:text-purple-900 transition-all"
+            >
               <GithubLogo size={32} />
             </a>
-            <a href="www.linkedin.com/in/levid-lima-326311260" className="hover:text-purple-900 transition-all">
+            <a
+              href="www.linkedin.com/in/levid-lima-326311260"
+              className="hover:text-purple-900 transition-all"
+            >
               <LinkedinLogo size={32} />
             </a>
           </div>
@@ -87,6 +100,7 @@ export default function Contact() {
             <input
               required
               type="email"
+              name="email"
               id="email"
               placeholder="Digite seu email"
               className="bg-transparent border-2 p-3 rounded-lg"
@@ -100,19 +114,25 @@ export default function Contact() {
               Nome
             </label>
             <input
+              required
               type="text"
+              name="name"
               id="name"
               placeholder="Digite seu nome"
               className="bg-transparent border-2 p-3 rounded-lg"
             />
           </div>
-          <textarea
-            required
-            name="message"
-            id="message"
-            placeholder="Digite sua mensagem aqui..."
-            className="bg-transparent h-20 border-2 p-2 rounded-lg overflow-auto"
-          ></textarea>
+          <div className="flex flex-col relative">
+            <label htmlFor="message" className="absolute bg-gray-900 pl-2 pr-2 pt-0 pb-0 left-3 bottom-16">Mensagem</label>
+            <textarea
+              required
+              
+              name="message"
+              id="message"
+              placeholder="Digite sua mensagem aqui..."
+              className="bg-transparent h-20 border-2 p-4 rounded-lg overflow-auto resize-none"
+            />
+          </div>
 
           <button
             type="submit"
