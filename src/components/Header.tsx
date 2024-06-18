2409dfeb5caf0cca58cf52d0fd9c6f54b921251b
DEVLevid <levidlima80@gmail.com>
2024-06-18 16:09:36 -0300

    feat: Adding redirection animation for page.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,31 @@
 import { Code, GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
-import { useState } from "react";
+import { MouseEvent } from "react";
 
 export default function Header() {
-    const [route, setRoute] = useState('#')
+    const handleScroll = (event: MouseEvent<HTMLAnchorElement>, sectionId: string) => {
+        event.preventDefault();
+        const section = document.querySelector(sectionId);
+        const offset = 80; 
+        if (section) {
+            const sectionTop = section.getBoundingClientRect().top + window.pageYOffset - offset;
+            window.scrollTo({ top: sectionTop, behavior: "smooth" });
+        }
+    };
+
     return (
         <header className="w-full h-14 bg-black flex items-center justify-between pl-72 pr-72 shadow-md fixed top-0 z-10">
-            <a href="/" onClick={()=> setRoute(route)} className="text-2xl flex flex-row items-center gap-2"><Code size={28} />Lima</a>
+            <a href="/" className="text-2xl flex flex-row items-center gap-2"><Code size={28} />Lima</a>
             <ul className="text-center flex flex-row gap-20 text-xl">
-                <li><a href="#home" onClick={()=> setRoute('#home')} className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Início</a></li>
-                <li><a href="#about" onClick={()=> setRoute('#about')} className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Sobre mim</a></li>
-                <li><a href="#services" onClick={()=> setRoute('#services')}className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Serviços</a></li>
-                <li><a href="#portfolio" onClick={()=> setRoute('#portfolio')} className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Projetos</a></li>
-                <li><a href="#contact" onClick={()=> setRoute('#contact')} className=" border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Contato</a></li>
+                <li><a href="#home" onClick={(e) => handleScroll(e, '#home')} className="border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Início</a></li>
+                <li><a href="#about" onClick={(e) => handleScroll(e, '#about')} className="border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Sobre mim</a></li>
+                <li><a href="#services" onClick={(e) => handleScroll(e, '#services')} className="border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Serviços</a></li>
+                <li><a href="#portfolio" onClick={(e) => handleScroll(e, '#portfolio')} className="border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Projetos</a></li>
+                <li><a href="#contact" onClick={(e) => handleScroll(e, '#contact')} className="border-b-2 border-transparent hover:border-purple-600 transition-colors delay">Contato</a></li>
             </ul>
             <div className="flex flex-row gap-2">
                 <a href="https://github.com/DEVLevid"><GithubLogo size={32} className="hover:text-purple-600 cursor-pointer transition-colors delay" /></a>
                 <a href="https://www.linkedin.com/in/levid-lima-326311260"><LinkedinLogo size={32} className="hover:text-purple-600 cursor-pointer transition-colors delay" /></a>
             </div>
         </header>
-    )
+    );
 }
